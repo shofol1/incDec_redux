931e748ab5177fec5afc816ea27734efa6f94f73
shofol <18103156@iubat.edu>
2022-03-01 23:32:06 +0600

    redux action dispatch reducer

---
+++ src/Action/index.js
@@ -0,0 +1,7 @@
+export const incNum=()=>{
+    return{type:'INCREMENT'}
+};
+
+export const decNum=()=>{
+    return{type:'DECREMENT'}
+};
---
+++ src/Reducer/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import changeNum from "./incDec";
+
+
+const rootReducer=combineReducers({changeNum});
+
+export default rootReducer;

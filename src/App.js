d401f8aff44d3c950bb72b60af13fce16b3e160f
shofol <18103156@iubat.edu>
2022-03-01 22:52:15 +0600

    redux practise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
+import store from './store';
+import { useDispatch } from 'react-redux';
+import { decNum, incNum } from './Action';
 
+
+store.subscribe(()=>console.log(store.getState()))
 function App() {
+  const myState=useSelector(state=>state.changeNum)
+  const dispatch=useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{myState}</h1>
+     <button onClick={()=>dispatch(incNum())}>Increament</button>
+     <button onClick={()=>dispatch(decNum())}>decreament</button>
     </div>
   );
 }
